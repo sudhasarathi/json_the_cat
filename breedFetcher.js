03a9911cd8759d473c5ffe494fed7102331b96bf
sudhasarathi <sudhav158@email.com>
2023-01-19 17:00:43 -0700

    2nd assignment

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,8 +1,7 @@
 const request = require('request');
-const breedName = process.argv[2];
-console.log(breedName);
 
-// const fetchBreedDescription = function(breedName, callback) {
+
+const fetchBreedDescription = function(breedName, callback) {
 
   const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
   request(url, (error, resp, body) => {
@@ -10,21 +9,21 @@ console.log(breedName);
     // console.log(typeof body);
 
     if (error) {
-     return console.log("failed request", error);
+      return callback(error, null);
     }
 
     const data = JSON.parse(body);
-    // console.log(data);
+    console.log(data);
 
     const breed = data[0];
     if (breed) {
-      console.log(breed.description);
+      callback(null,breed.description);
     } else {
-      console.log("breed not found");
+      callback("breed not found");
     }
   });
 
-// };
+};
 
-// module.exports = fetchBreedDescription;
+module.exports = {fetchBreedDescription};
 
